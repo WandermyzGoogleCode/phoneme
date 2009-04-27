dac6436bb20380d09ebd3498b19e6763ca363f28
lijingweb <lijingweb@5bb5af84-0706-11de-93dd-cf879171209e>
2009-04-27 11:58:22 +0000

    2009-4-27：work with architecture,partI not finished yet

---
+++ trunk/src/ui/Gui.java
@@ -0,0 +1,16 @@
+package ui;
+import entity.ResulType;
+public class Gui {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public void pushResult(ResulType type,Object o){
+		//
+	}
+
+}
