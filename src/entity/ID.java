fe81c785bfeb8fafe8a84b0e3d67cbc8f43c9134
LiYuqian79 <LiYuqian79@5bb5af84-0706-11de-93dd-cf879171209e>
2009-05-22 08:47:43 +0000

    调试

--- src/entity/ID.java
+++ src/entity/ID.java
@@ -1,12 +1,18 @@
 package entity;
 
+import java.io.Serializable;
+
 /**
  * 用来标识用户或者群组的唯一标志，只有系统可见，用户不可见
  * 该ID由系统的IDFactory自动生成，
  * @author Administrator
  *
  */
-public class ID {
+public class ID implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3866497586725147310L;
 	private int id;
 	
 	static ID getNullID()
