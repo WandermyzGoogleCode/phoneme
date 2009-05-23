cde56bd86e1b69431d49a8f969e9758f09bd74db
yangzj81999 <yangzj81999@5bb5af84-0706-11de-93dd-cf879171209e>
2009-05-23 14:10:59 +0000

    在Birthday中增加了返回int的年月日功能

--- trunk/src/entity/infoField/Birthday.java
+++ trunk/src/entity/infoField/Birthday.java
@@ -8,11 +8,18 @@ package entity.infoField;
 public class Birthday extends EmptyBirthday {
 	private String day;
 	private int y, m, d;
+	private final int yyyy=0;
+	private final int mm=1;
+	private final int dd=2;
 
 	public Birthday(String birthday)
 	{
 		//ºÏ≤È∏Ò Ω
 		//TODO 
+		String date[]=birthday.split("-");
+		this.y=Integer.parseInt(date[yyyy]);
+		this.m=Integer.parseInt(date[mm]);
+		this.d=Integer.parseInt(date[dd]);
 		this.day=birthday;
 	}
 	
