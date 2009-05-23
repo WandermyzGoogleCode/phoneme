24ce6c0bbd38a4bd2cbe7da3de2b8ae98e7bfb5c
LiYuqian79 <LiYuqian79@5bb5af84-0706-11de-93dd-cf879171209e>
2009-05-23 13:55:52 +0000

    增加了获取年、月、日

--- src/entity/infoField/Birthday.java
+++ src/entity/infoField/Birthday.java
@@ -1,7 +1,13 @@
 package entity.infoField;
 
+/**
+ * 传入的时候，必须是yyyy-mm-dd，否则格式不合法
+ * @author Administrator
+ *
+ */
 public class Birthday extends EmptyBirthday {
 	private String day;
+	private int y, m, d;
 
 	public Birthday(String birthday)
 	{
@@ -20,5 +26,17 @@ public class Birthday extends EmptyBirthday {
 	@Override
 	public String getStringValue() {
 		return day;
+	}
+	
+	public int getYear(){
+		return y;
+	}
+	
+	public int getMonth(){
+		return m;
+	}
+	
+	public int getDay(){
+		return d;
 	}
 }
